bef9fc6984f201c2365bdfaa871dd5b892eb046e
Ian Heinrich <ian@heinrich.org.uk>
2020-12-08 07:41:45 -0500

    updt

--- android/src/main/java/com/github/rmtmckenzie/qrmobilevision/QrCamera.java
+++
@@ -1,9 +0,0 @@
-package com.github.rmtmckenzie.qrmobilevision;
-
-interface QrCamera {
-    void start() throws QrReader.Exception;
-    void stop();
-    int getOrientation();
-    int getWidth();
-    int getHeight();
-}
